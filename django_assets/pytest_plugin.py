435264eb704ebf1adbfb5d04b4e0e6eceb5c30f0
Chris Wilson <chris+github@aptivate.org>
2014-04-29 12:22:10 +0100

    Add plugin that initialises django_assets in pytest
    This ensures that django_assets configuration settings are not removed after
    each pytest test run.

---
+++ django_assets/pytest_plugin.py
@@ -0,0 +1,7 @@
+import pytest
+import django_assets.env
+
+@pytest.fixture(autouse=True)
+def set_django_assets_env():
+    print "Set django assets environment"
+    django_assets.env.get_env() # initialise django-assets settings
