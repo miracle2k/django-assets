2d61e519e236afa2ec9c0a815383db02d546d711
Michael Elsdörfer <michael@elsdoerfer.com>
2014-06-15 05:40:30 +0200

    Depend on most recent stable webassets.
    Same as in flask-assets, we no longer feasably depend on non-PyPi
    hosted packages.

--- django_assets/__init__.py
+++ django_assets/__init__.py
@@ -6,7 +6,7 @@ from django_assets.env import register
 __all__ = ('Bundle', 'register')
 
 __version__ = (0, 9)
-__webassets_version__ = ('dev',)
+__webassets_version__ = ('0.10',)
 
 
 from django_assets import filter
