d7e091805fca19edde665034feef200f0cd3c6e1
Michael Elsdörfer <michael@elsdoerfer.com>
2014-06-30 02:39:35 +0200

    Make staticfile use dependent on ASSETS_DEBUG.
    Part of #42. Close #3, #30.

--- django_assets/env.py
+++ django_assets/env.py
@@ -110,7 +110,7 @@ class DjangoResolver(Resolver):
 
     @property
     def use_staticfiles(self):
-        return settings.DEBUG and \
+        return settings.ASSETS_DEBUG and \
             'django.contrib.staticfiles' in settings.INSTALLED_APPS
 
     def glob_staticfiles(self, item):
