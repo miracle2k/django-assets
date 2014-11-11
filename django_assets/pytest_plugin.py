9db6e5fdc40b4fc58dcf5b34eeb7cc9a60983f28
Jason Pleau <jason@jpleau.ca>
2014-11-10 22:14:52 -0500

    pytest_plugin: python3 compat for print()
    trivial change to the print statement to make it work on python3

--- django_assets/pytest_plugin.py
+++ django_assets/pytest_plugin.py
@@ -3,5 +3,5 @@ import django_assets.env
 
 @pytest.fixture(autouse=True)
 def set_django_assets_env():
-    print "Set django assets environment"
+    print("Set django assets environment")
     django_assets.env.get_env() # initialise django-assets settings
