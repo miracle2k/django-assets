002e903af8632650096838ec2f26048b1e83ebe4
Michael Elsdörfer <michael@elsdoerfer.info>
2019-12-20 18:40:12 +0100

    Update version.

--- django_assets/__init__.py
+++ django_assets/__init__.py
@@ -5,8 +5,8 @@ from django_assets.env import register
 
 __all__ = ('Bundle', 'register')
 
-__version__ = (0, 12)
-__webassets_version__ = ('>=0.11',)
+__version__ = (2, 0)
+__webassets_version__ = ('>=2.0',)
 
 
 from django_assets import filter
