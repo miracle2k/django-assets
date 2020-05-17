239c6040f9da3cfd8045ef101879fa54ff2751e3
John Vandenberg <jayvdb@gmail.com>
2020-05-17 10:12:28 +0700

    Fix invalid escape sequence in regex
    Fixes https://github.com/miracle2k/django-assets/issues/98

--- django_assets/glob.py
+++ django_assets/glob.py
@@ -121,7 +121,7 @@ def translate(pat):
                 res = '%s([%s])' % (res, stuff)
         else:
             res = res + re.escape(c)
-    return res + '\Z(?ms)'
+    return res + r'\Z(?ms)'
 
 
 """Filename globbing utility."""
