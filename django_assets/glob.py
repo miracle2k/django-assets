e57c5c3c02d84069600916fed74f165819393487
Jonatan Heyman <jonatan@heyman.info>
2022-11-10 23:26:07 +0100

    Global regex flags must be at the start of regex in Python 3.11

--- django_assets/glob.py
+++ django_assets/glob.py
@@ -121,7 +121,7 @@ def translate(pat):
                 res = '%s([%s])' % (res, stuff)
         else:
             res = res + re.escape(c)
-    return res + '\Z(?ms)'
+    return '(?ms)' + res + '\Z'
 
 
 """Filename globbing utility."""
